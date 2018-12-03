77ef9798d8c089b1e5d2c10fb6d5383b02db713d
Ken Gardiner <kagemail@gmail.com>
2018-12-03 16:20:43 -0500

    css changes

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,18 @@
+// import React from "react";
+
+// class Home extends React.component {
+//     render() {
+//         return <p>Hey!</p>
+//     }
+// }
+
+import Link from 'next/link';
+
+// we don't need a full component, let's just do a stateless functional component
+const Home = props => (
+    <div>
+        <p>Hey!</p>
+    </div>
+);
+
+export default Home;
